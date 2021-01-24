fa23c8a9c406256a6154779adcdf497d31a0d471
Moody Rahman <moody.byte@gmail.com>
2021-01-23 19:11:13 -0500

    error test route

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,18 @@
 from flask import *
-
-
+from .utils.decorators import errorhandle
 
 app = Flask(__name__)
 
 
+@app.route("/")
+def index():
+	return {
+		"status":200,
+		"message":"api is active!"
+	}
+
+@app.route("/error")
+@errorhandle
+def error():
+	raise Exception("death")
+	return "something bad"
